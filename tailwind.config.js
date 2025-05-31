24ce62f7a3bf6c4925fe0ad7aed478382094f31b
Amine <126218968+AmineChafii@users.noreply.github.com>
2025-06-01 00:31:01 +0100

    Configuration de l'environnement moderne

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./view/**/*.{php,html,js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#042C46",
+        secondary: "#BA6932",
+        accent: "#EFEEDC"
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        display: ['Montserrat', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+}
