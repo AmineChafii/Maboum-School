0f5d6cca22373c5984d6c71c681d3b39a3ee0274
Amine <126218968+AmineChafii@users.noreply.github.com>
2025-06-01 01:03:01 +0100

    Set up project with Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,39 @@
 module.exports = {
   content: [
     "./view/**/*.{php,html,js}",
+    "./src/**/*.{php,html,js}"
   ],
   theme: {
     extend: {
       colors: {
-        primary: "#042C46",
-        secondary: "#BA6932",
-        accent: "#EFEEDC"
+        primary: {
+          50: '#f0f9ff',
+          100: '#e0f2fe',
+          200: '#bae6fd',
+          300: '#7dd3fc',
+          400: '#38bdf8',
+          500: '#0ea5e9',
+          600: '#0284c7',
+          700: '#0369a1',
+          800: '#075985',
+          900: '#0c4a6e',
+        },
+        secondary: {
+          50: '#fdf8f6',
+          100: '#f2e8e5',
+          200: '#eaddd7',
+          300: '#e0cec7',
+          400: '#d2bab0',
+          500: '#bfa094',
+          600: '#a18072',
+          700: '#977669',
+          800: '#846358',
+          900: '#43302b',
+        },
       },
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
-        display: ['Montserrat', 'sans-serif']
+        serif: ['Merriweather', 'serif'],
       }
     }
   },
